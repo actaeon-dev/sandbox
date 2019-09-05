0c273fe0735b14d794be178f7db31faa8d819e5c
Cat Stevens <catb0t@protonmail.ch>
2019-09-04 21:55:03 -0400

    ?

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,66 @@
-import * as i10n from './i10n';
+// import * as  i18n from './ i18n';
+
+/** a */
+const  i18n_document_template = 'data/i18n/%s.json';
+/** a */
+const  i18n_root_document = 'i18n';
+
+/**
+ * Blah
+ */
+interface I18nManifest {
+  /** a */
+  available : string[];
+}
+
+/** a  */
+
+async function i18n_get_available_translations () : Promise<string[]> {
+  return fetch( i18n_document_template.replace('%s', i18n_root_document) )
+    .then( (response : Response) : { } =>
+      response.json()
+        .then( (x : { }) => <I18nManifest> x ) )
+    .then( (manifest : I18nManifest) : string[] => manifest.available );
+}
+
+/**
+ * Blah
+ */
+function i18n_translate_page () : void {
+  // populate all class=translatable nodes given their data- i18n-id value
+}
+
+/** a */
+function createElement (name : string) : Element {
+  return (window.document.createElement(name) || ( () : never => {
+    throw new Error('can\'t create new element: ' + name); }) );
+}
+
+/**
+ * Blah
+ */
+function  i18n_populate_translation_selection () : void {
+  // const data : string[] = async () => i18n_get_available_translations();
+  // const sel: HTMLSelectElement =
+  //  <HTMLSelectElement> getElementById(' i18n-selection');
+  // for each locale listed in data/ i18n/ i18n.json, then add it as an
+  //  <option> to the selector
+
+  const available : string[] = i18n_get_available_translations()
+    .then((x : string[]) : string[] => x )
+    .catch((err : TypeError) : never => { throw err; } );
+
+  const i18n_selection = getElementById('i18n-selection');
+
+  for (const locale of available) {
+    const option : HTMLOptionElement = <HTMLOptionElement> createElement('option');
+    option.setAttribute('id', 'locale-' + locale);
+    option.setAttribute('name', locale);
+    option.appendChild(window.document.createTextNode( locale) );
+
+    i18n_selection.insertAdjacentElement( 'beforeend', option );
+  }
+}
 
 /** a */
 function getElementById (id : string) : HTMLElement {
@@ -40,16 +102,15 @@ function make_listeners () : void {
   getElementById('scroll-input')
     .addEventListener('change', <EventListener> change_scroll_to_anchor);
 
-  getElementById('i10n-selection')
-    .addEventListener('change', <EventListener> i10n.translate_page);
+  getElementById('i18n-selection')
+    .addEventListener('change', <EventListener>  i18n_translate_page);
 }
 
 /** a */
 function main () : void {
   console.log('hello');
   make_listeners();
-  i10n.populate_translation_selection();
-  console.log(i10n.get_available_translations());
+  i18n_populate_translation_selection();
 }
 
 window.onload = main;
