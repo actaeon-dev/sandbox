02d0222849fc02213c4f304bba10a1e45fffa76d
Cat Stevens <catb0t@protonmail.ch>
2019-09-09 22:47:13 -0400

    etc

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import * as i18n          from './managers/i18n';
 import * as prefs         from './managers/prefs';
 
 /** change which line the scroll-to anchor is a link */
-function change_scroll_to_anchor () : void {
+function change_scroll_to_anchor (e : InputEvent) : void {
+  e.preventDefault();
   const scroll_input = <HTMLInputElement> dom.getElementById('scroll-input');
 
   const scrollpos : number = Math.abs(Number(scroll_input.value) - 1);
@@ -16,19 +17,21 @@ function change_scroll_to_anchor () : void {
 /** create event listeners for page elements */
 async function
 make_listeners (lh : i18n.LocaleHandler, gm : game_manager.GameState) : Promise<void> {
+    // TODO: move to GameState
   (() => {
     const input_listeners : string[] = ['keydown', 'keypress', 'keyup'];
 
-    const primary = <HTMLInputElement> dom.getElementById('primary');
+    const stdin = <HTMLInputElement> dom.getElementById('stdin');
 
     for (const l of input_listeners) {
-      primary.addEventListener(l, <EventListener> gm.handle_keyboard.bind(gm));
+      stdin.addEventListener(l, <EventListener> gm.handle_keyboard.bind(gm));
     }
   })();
 
   dom.getElementById('scroll-input')
     .addEventListener('change', <EventListener> change_scroll_to_anchor);
 
+  // TODO: move to Preferences
   dom.getElementById('i18n-selection')
     // why is .bind necessary? it's already called on lh, a LocaleHandler() instance
     //   NOT the LocaleHandler typename
@@ -39,7 +42,8 @@ make_listeners (lh : i18n.LocaleHandler, gm : game_manager.GameState) : Promise<
     dom.getElementById('prefs-controls-sub').style.display = 'none';
 
     dom.getElementById('prefs-visibility')
-      .addEventListener('click', <EventListener> (() => {
+      .addEventListener('click', <EventListener> ((e : MouseEvent) => {
+        e.preventDefault();
         const s = dom.getElementById('prefs-controls-sub');
         s.style.display = (s.style.display === 'none')
           ? 'inherit'
--- src/managers/game.ts
+++ src/managers/game.ts
@@ -1,18 +1,30 @@
+import * as dom from '../lib/dom';
 // you will write localstorage with 'game:' namespace
 
 /** need to be able to save/load states, from client or server */
 export class GameState {
 
   /** a */
-  private last_kb_evt : KeyboardEvent;
+  // private last_kb_evt : KeyboardEvent;
 
-  constructor () {
-    this.last_kb_evt = new KeyboardEvent('a');
+/*  constructor () {
+    this.last_kb_evt = new KeyboardEvent('');
   }
-
+*/
   /** a */
   public handle_keyboard (kbe : KeyboardEvent) : void {
-    console.log('previous last evt: ', this.last_kb_evt);
-    this.last_kb_evt = kbe;
+    // console.log('previous last evt: ', this.last_kb_evt);
+    // this.last_kb_evt = kbe;
+    const input = (<HTMLInputElement> dom.getElementById('stdin')).value;
+
+    if (kbe.isTrusted) {
+      if (kbe.key === 'Enter') {
+        console.log('user submit: ', input);
+        //
+      }
+    } else {
+      // scripted
+    }
   }
+
 }
